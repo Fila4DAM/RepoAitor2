7c153a4207586e154c35516adaf2524c9509289b
admin1 <admin1@PC1444>
2017-01-10 08:10:19 +0100

    aaaaa

--- pruebagitorganizacion/src/pruebagitorganizacion/prueba.java
+++ pruebagitorganizacion/src/pruebagitorganizacion/prueba.java
@@ -1,5 +1,14 @@
 package pruebagitorganizacion;
 
-public class prueba {
+import java.util.Scanner;
 
+public class prueba {
+	public static void main(String[] args){
+		int numero=0;
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Escribir un número");
+		numero=teclado.nextInt();
+		System.out.println("El cuadrado del número"+numero+"es: "+numero*numero);
+	}
 }
+
